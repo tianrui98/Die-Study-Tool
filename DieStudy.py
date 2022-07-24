345488a79de74e907be903bd1ab98c3b39877ae8
Tian Rui <zhutianrui1998@gmail.com>
2022-07-24 16:56:19 +0800

    ready to start revamping stage 0

--- DieStudy.py
+++ DieStudy.py
@@ -28,7 +28,6 @@ def test_main():
 if __name__ == "__main__":
     test_main()
 
-
 #todo
 #1. add save midway - done
 #2. make it easier to share restored data (path issue) - done
@@ -36,5 +35,5 @@ if __name__ == "__main__":
 #4. when user reaches the end of cluster but skipped some images. direct them to those skipped images
 #5. Reduce repetitive comparison - done
 #6. Add delete project button
-#7. when save progress, save all matches and no matches of the current cluster
-#8. Only save progress (write to data.json) when the user clicks save button
+#7. when save progress, save all matches and no matches of the current cluster - done
+#8. Only save progress (write to data.json) when the user clicks save button - done
