3b31851015cccb1acc0510ca1368878af52f3280
Tian Rui <zhutianrui1998@gmail.com>
2022-08-05 15:56:45 +0700

    clean up test folder

--- DieStudy.py
+++ DieStudy.py
@@ -6,17 +6,6 @@ from src.data_to_folder import *
 def main():
     UI().start()
 
-def test_identical():
-    """
-    Manual testing
-    """
-    test_folder_address = os.path.join("test", "test_identical_long")
-    test_cluster_address = os.path.join(test_folder_address, "test_cluster")
-    test_data_address = os.path.join(test_folder_address, "test_data.json")
-    progress_data, stage, cluster = load_progress(test_cluster_address, True, test_data_address)
-    UI = UI(project_name="test", project_address=test_cluster_address, progress_data= progress_data, cluster = cluster, stage = stage)
-    UI.start()
-
 def test_main():
     """
     Semi-automated testing. User still has to click the buttons but the actions will be recorded and compared with progress_data
@@ -25,4 +14,4 @@ def test_main():
     UI(testing_mode = True).start()
 
 if __name__ == "__main__":
-    test_main()
+    main()
