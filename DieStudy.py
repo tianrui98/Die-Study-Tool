4f2b108831790ef8d598a8739419cc9736a45ca7
Tian Rui <77308055+tianrui98@users.noreply.github.com>
2024-02-05 13:35:10 -0500

    working on export format

--- DieStudy.py
+++ DieStudy.py
@@ -3,7 +3,7 @@ import argparse
 from reset import *
 from src.root_logger import *
 
-__version__ = 20240119
+__version__ = 20240205
 logger.info(f"Latest update: {__version__}")
 
 parser = argparse.ArgumentParser()
